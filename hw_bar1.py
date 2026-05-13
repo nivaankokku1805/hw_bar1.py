eafbc1f2d7f06f8e48fa70e339af00279789568f
nivaankokku1805 <reetu18@gmail.com>
2026-05-13 11:48:09 +0530

    Add files via upload

---
+++ hw_bar1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+
+countries_df = pd.read_csv('countries.csv')
+countries = countries_df
+
+print(countries.head())
+
+
+31889923 - 8425333
+
+countries.shape, type(countries)
+countries.head(10)
+
+names = ['United States', 'India', 'China', 'Indonesia', 'Brazil', 'Pakistan', 'Nigeria', 'Bangladesh', 'Russia', 'Mexico', 'Japan', 'Ethiopia', 'Philippines', 'Egypt', 'Vietnam', 'Germany', 'Turkey', 'Iran', 'Thailand', 'United Kingdom', 'France', 'Italy', 'Myanmar', 'South Africa', 'South Korea', 'Colombia', 'Spain', 'Ukraine', 'Argentina', 'Algeria', 'Sudan', 'Iraq', 'Afghanistan', 'Poland', 'Canada', 'Morocco', 'Saudi Arabia', 'Uzbekistan', 'Peru', 'Venezuela']
